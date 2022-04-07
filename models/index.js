9dd9c6679b826367b6ad8a68da078ff687c42bee
SrGiovanni <samuelefulghi@gmail.com>
2022-04-07 19:15:48 -0400

    sequalize seems to not work

--- models/index.js
+++ models/index.js
@@ -5,13 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
+
+// TODO: seek clarity on this part
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+})
 
+/* 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey:'id'
+})
+ */
+// /TODO get clarity on the last part
 module.exports = {
   Product,
   Category,
